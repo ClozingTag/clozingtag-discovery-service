b960fe6246aa963c83481081aad7c3b31458ed74
Saint Deemene <deemene.saint@loblaw.ca>
2025-03-04 11:37:55 +0000

    project init
    Project Setup

---
+++ src/main/java/com/clozingtag/clozingtag/discovery/service/ClozingtagDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.discovery.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClozingtagDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClozingtagDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clozingtag/clozingtag/discovery/service/ClozingtagDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clozingtag.clozingtag.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClozingtagDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
